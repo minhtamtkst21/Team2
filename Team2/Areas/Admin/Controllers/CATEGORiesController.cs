2f3a6b77c1ed7439e834751339a20e81f54bc761
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2020-11-20 21:29:14 +0700

    Add project files.

---
+++ Team2/Areas/Admin/Controllers/CATEGORiesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using Team2.Models;
+
+namespace Team2.Areas.Admin.Controllers
+{
+    public class CATEGORiesController : Controller
+    {
+        private QUANLYCANTEENEntities db = new QUANLYCANTEENEntities();
+
+        // GET: Admin/CATEGORies
+        public ActionResult Index()
+        {
+            return View(db.CATEGORies.ToList());
+        }
+
+        // GET: Admin/CATEGORies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORY cATEGORY = db.CATEGORies.Find(id);
+            if (cATEGORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORY);
+        }
+
+        // GET: Admin/CATEGORies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/CATEGORies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CATEGORY cATEGORY, HttpPostedFileBase Picture)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Picture != null)
+                {
+                    using (var scope = new TransactionScope())
+                    {
+                        db.CATEGORies.Add(cATEGORY);
+                        db.SaveChanges();
+
+                        var path = Server.MapPath(PICTURE_PATH);
+                        Picture.SaveAs(path + cATEGORY.ID);
+
+                        scope.Complete();
+                        return RedirectToAction("Index");
+                    }
+                }
+                else ModelState.AddModelError("", "Picture not found!");
+            }
+            return View(cATEGORY);
+        }
+
+        // GET: Admin/CATEGORies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORY cATEGORY = db.CATEGORies.Find(id);
+            if (cATEGORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORY);
+        }
+
+        // POST: Admin/CATEGORies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CATEGORY_CODE,CATEGORY_NAME,IMAGE_URL,STATUS")] CATEGORY cATEGORY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cATEGORY).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cATEGORY);
+        }
+
+        // GET: Admin/CATEGORies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORY cATEGORY = db.CATEGORies.Find(id);
+            if (cATEGORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORY);
+        }
+
+        // POST: Admin/CATEGORies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CATEGORY cATEGORY = db.CATEGORies.Find(id);
+            db.CATEGORies.Remove(cATEGORY);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Image(int id)
+        {
+            var path = Server.MapPath(PICTURE_PATH);
+            return File(path + id, "images");
+        }
+        private const string PICTURE_PATH = "~/Images/Categories/";
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
