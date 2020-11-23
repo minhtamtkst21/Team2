3a82bf25e47a236c878fa021e046df97d7b3ef04
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2020-11-24 06:49:01 +0700

    PUSHCODE

--- Team2/Areas/Admin/Controllers/AdminHomeController.cs
+++ Team2/Areas/Admin/Controllers/AdminHomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Team2.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class AdminHomeController : Controller
     {
         // GET: Admin/Home
         public ActionResult Index()
