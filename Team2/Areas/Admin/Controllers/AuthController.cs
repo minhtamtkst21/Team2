fbf386318dc38682846a84273149b0739c9e10c4
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2020-12-01 07:17:12 +0700

    Login Admin

---
+++ Team2/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team2.Models;
+
+namespace Team2.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        QUANLYCANTEENEntities model = new QUANLYCANTEENEntities();
+        // GET: Admin/Auth
+        public ActionResult Login()
+        {
+            Session["password-incorrect"] = false;
+            Session["user-not-found"] = false;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string pass)
+        {
+            Session["password-incorrect"] = false;
+            Session["user-not-found"] = false;
+            Session["user-fullname"] = false;
+            Session["user-id"] = false;
+            Session["user-role"] = false;
+            var user = model.ACCOUNTs.FirstOrDefault(s => s.EMAIL.Equals(email));
+            if (user != null)
+            {
+                if (user.PASSWORD.Equals(pass))
+                {
+                    Session["user-fullname"] = user.FULL_NAME;
+                    Session["user-id"] = user.ID;
+                    Session["user-role"] = user.ROLE;
+                    return RedirectToAction("Index", "AdminHome");
+                }
+                else
+                {
+                    Session["password-incorrect"] = true;
+                    return View();
+                }
+            }
+            Session["user-not-found"] = true;
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session["user-fullname"] = null;
+            Session["user-id"] = null;
+            return RedirectToAction("Login");
+        }
+    }
+}
